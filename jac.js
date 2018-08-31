0d9246b9e21fdab2f721c114eb9bbf8d58a654b3
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-31 23:52:41 +0300

    Update jac.js

--- jac.js
+++ jac.js
@@ -8,7 +8,7 @@ const jimp = require('jimp');
 
 const Canvas = require('canvas');
 
- 
+ const clientt = new Discord.Client();
 
 const client = new Discord.Client();
 
@@ -111,6 +111,56 @@ client.on("guildMemberAdd", (member) => {
 });
 
 
+
+
+
+const adminprefixx = "#";
+
+const devs = ['470500980342128650'];
+
+client.on('message', message => {
+
+  var argresult = message.content.split(` `).slice(1).join(' ');
+
+    if (!devs.includes(message.author.id)) return;
+
+if (message.content.startsWith(adminprefixx + 'setplaying')) {
+
+  client.user.setGame(argresult);
+
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+
+} else
+
+  if (message.content.startsWith(adminprefixx + 'setname')) {
+
+client.user.setUsername(argresult).then
+
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+
+} else
+
+  if (message.content.startsWith(adminprefixx + 'setavatar')) {
+
+client.user.setAvatar(argresult);
+
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+
+      } else
+
+if (message.content.startsWith(adminprefixx + 'setT')) {
+
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+
+}
+
+});
+
+clientt.login(process.env.BOT_TOKEN); 
 
 
 client.login(process.env.BOT_TOKEN); 
