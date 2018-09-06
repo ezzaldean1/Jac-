adabd075baa446caa4bc90418840bb40b4d682c3
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-09-06 16:14:55 +0300

    Create jac.js

---
+++ jac.js
@@ -0,0 +1,53 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+
+  console.log('---------------');
+
+  console.log(' Mob2 Bot Is Online')
+
+  console.log('---------------')
+
+  client.user.setStatus("Online")
+
+    client.user.setActivity(" United Server ",{type: 'PLAYING'})
+
+	});
+
+            var prefix = "." ;
+
+            
+
+
+
+
+client.on ("guildMemberAdd", member => {
+
+  
+
+   var role = member.guild.roles.find ("name",  "United");
+
+   member.addRole (role);
+
+  
+
+})
+
+client.on ("guildMemberRemove", member => {
+
+   
+
+})
+
+client.on ("guildMemberRemove", member => {
+
+   
+
+})
+
+
+
+
+      
